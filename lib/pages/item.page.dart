3e282479521ef0eeb3cef9bbc52c547f173a3088
Caio Neves <ncaio037@gmail.com>
2023-05-28 17:31:05 -0300

    Implementing the items page card and consuming the api to validate user login.

--- lib/pages/item.page.dart
+++ lib/pages/item.page.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
-class NovaPagina extends StatelessWidget {
+class NewPage extends StatefulWidget {
+  @override
+  _NewPageState createState() => _NewPageState();
+}
+
+class _NewPageState extends State<NewPage> {
+  List<Item> Itens = [
+    Item(name: 'ITEM MENU 01', icon: Icons.check_box),
+    Item(name: 'ITEM MENU 02', icon: Icons.calendar_month),
+    Item(name: 'ITEM MENU 03', icon: Icons.refresh),
+    Item(name: 'ITEM MENU 04', icon: Icons.lock),
+    Item(name: 'ITEM MENU 05', icon: Icons.car_crash),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Nova Página'),
+        title: Text('MOBI'),
         backgroundColor: Color.fromARGB(255, 36, 36, 36),
       ),
       drawer: Drawer(
@@ -36,25 +49,46 @@ class NovaPagina extends StatelessWidget {
                 ],
               ),
             ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-              },
-            ),
+            for (int i = 0; i < Itens.length; i++)
+              ListTile(
+                leading: Icon(Itens[i].icon),
+                title: Text(Itens[i].name),
+                onTap: () {
+                  setState(() {
+                    Itens[i].icon = Icons.lock_open;
+                  });
+                },
+              ),
           ],
         ),
       ),
-      body: Center(
-        child: Text(
-          'Conteúdo da nova página',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 1,
+        padding: EdgeInsets.all(25),
+        children: [
+          for (int i = 0; i < Itens.length; i++)
+            Card(
+              color: Color.fromARGB(255, 36, 36, 36),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Center(
+                child: ListTile(
+                  leading: Icon(Itens[i].icon), iconColor: Colors.yellow,
+                  title: Text(Itens[i].name,), textColor: Colors.white,
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
+}
+
+class Item {
+  String name;
+  IconData icon;
+
+  Item({required this.name, required this.icon});
 }
--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'item.page.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -11,155 +13,190 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   bool visibilityPassword = false;
   bool visibilityRememberAutent = true;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  Future<void> login() async {
+    final url = 'https://desafio-nodejs.herokuapp.com/user/login';
+
+    var body = json.encode({
+      'email': emailController.text,
+      'password': passwordController.text
+    });
+
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => NewPage()),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Login Failed'),
+            content: Text('Invalid email or password'),
+            actions: [
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 242, 242, 242),
-        body: Padding(
-            padding: EdgeInsets.all(10),
-            child: Center(
-              child: SingleChildScrollView(
-                // Adicionado SingleChildScrollView
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        width: double.infinity,
-                        padding: EdgeInsets.only(bottom: 60.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              'MOBI',
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 56, 56, 56),
-                                fontSize: 50.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              'LOGISTICA',
-                              style: TextStyle(
-                                color: Colors.yellow,
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
+      backgroundColor: const Color.fromARGB(255, 242, 242, 242),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.only(bottom: 60.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'MOBI',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 56, 56, 56),
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      TextFormField(
-                        autofocus: true,
-                        keyboardType: TextInputType.emailAddress,
-                        style: new TextStyle(color: Colors.black, fontSize: 20),
-                        decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 1.0),
-                          ),
-                          border: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 2.0),
-                          ),
-                          isDense: true,
-                          prefixIcon: Icon(
-                            Icons.person,
-                            color: Colors.black,
-                          ),
-                          labelText: "Email",
-                          labelStyle:
-                              TextStyle(color: Colors.black, fontSize: 20),
+                      Text(
+                        'LOGISTICA',
+                        style: TextStyle(
+                          color: Colors.yellow,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Divider(),
-                      TextFormField(
-                        keyboardType: TextInputType.visiblePassword,
-                        style: new TextStyle(color: Colors.black, fontSize: 20),
-                        decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Colors.black, width: 1.0)),
-                          border: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 2.0),
-                          ),
-                          isDense: true,
-                          suffixIcon: GestureDetector(
-                            child: Icon(
-                                visibilityPassword == false
-                                    ? Icons.visibility_off
-                                    : Icons.visibility,
-                                color: Colors.black),
-                            onTap: () {
-                              setState(() {
-                                visibilityPassword = !visibilityPassword;
-                              });
-                            },
-                          ),
-                          prefixIcon: Icon(
-                            Icons.lock,
-                            color: Colors.black,
-                          ),
-                          labelText: "Password",
-                          labelStyle:
-                              TextStyle(color: Colors.black, fontSize: 20),
-                        ),
-                        obscureText: visibilityPassword == false ? true : false,
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(5),
-                        child: Row(children: [
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                visibilityRememberAutent =
-                                    !visibilityRememberAutent;
-                              });
-                            },
-                            icon: Icon(
-                              visibilityRememberAutent
-                                  ? Icons.check_box
-                                  : Icons.check,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          Text(
-                            'Remember authentication',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ]),
-                      ),
-                      ElevatedButton(
+                    ],
+                  ),
+                ),
+                TextFormField(
+                  controller: emailController,
+                  autofocus: true,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 1.0),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    ),
+                    isDense: true,
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: Colors.black,
+                    ),
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                ),
+                Divider(),
+                TextFormField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.black, width: 1.0)),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    ),
+                    isDense: true,
+                    suffixIcon: GestureDetector(
+                      child: Icon(
+                          visibilityPassword == false
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                          color: Colors.black),
+                      onTap: () {
+                        setState(() {
+                          visibilityPassword = !visibilityPassword;
+                        });
+                      },
+                    ),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.black,
+                    ),
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                  obscureText: visibilityPassword == false ? true : false,
+                ),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  child: Row(
+                    children: [
+                      IconButton(
                         onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => NovaPagina(),
-                              ));
+                          setState(() {
+                            visibilityRememberAutent =
+                                !visibilityRememberAutent;
+                          });
                         },
-                        child: Text('LOGIN',
-                            style: TextStyle(color: Colors.white)),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              const Color.fromARGB(255, 36, 36, 36),
-                          elevation: 0,
+                        icon: Icon(
+                          visibilityRememberAutent
+                              ? Icons.check_box
+                              : Icons.check,
+                          color: Colors.grey,
                         ),
                       ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text('16:01:59 CHECK IN',
-                            style: TextStyle(color: Colors.black)),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.yellow,
-                          elevation: 0,
+                      Text(
+                        'Remember authentication',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.grey,
                         ),
-                      )
-                    ]),
-              ),
-            )));
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: login, // Chama a função login
+                  child: Text('LOGIN', style: TextStyle(color: Colors.white)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 36, 36, 36),
+                    elevation: 0,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('16:01:59 CHECK IN',
+                      style: TextStyle(color: Colors.black)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    elevation: 0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
